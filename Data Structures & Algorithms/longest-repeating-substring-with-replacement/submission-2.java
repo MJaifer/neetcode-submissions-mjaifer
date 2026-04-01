cebc3221b8883d41061f63248341e7a4442da7d9
Muhammed Jaifer M A <68530954+MJaifer@users.noreply.github.com>
2026-04-01 16:15:24 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l = 0, r = 0, maxLen = 0, maxF = 0;
+        int[] freq = new int[26];
+        while(r < s.length()) {
+            char cR = s.charAt(r);
+            freq[cR - 'A']++;
+            maxF = Math.max(maxF, freq[cR - 'A']);
+            
+
+            // invalid window, shrink from left
+            // window is invalid if (currentWindowLength) - (maxFrequency char in current window) is greater than k
+            // currentWindowLength = (r - l + 1) 
+            while (r - l + 1 - maxF - k > 0) {
+                char cL = s.charAt(l);
+                freq[cL - 'A']--;
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r - l + 1);
+            r++;
+        }
+        return maxLen;
+    }
+}
