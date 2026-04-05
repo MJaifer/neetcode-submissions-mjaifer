72cbdca4431a9d7db78990439b8ea9e4e4a2bf01
Muhammed Jaifer M A <68530954+MJaifer@users.noreply.github.com>
2026-04-05 20:59:15 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,26 @@
+class MinStack {
+    private Stack<int[]> stack;
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        int min = val;
+        if (!stack.isEmpty()) {
+            min = Math.min(min, stack.peek()[1]);
+        }
+        stack.push(new int[] {val, min});
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
